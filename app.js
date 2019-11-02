d86a2a22fdc895b648f7f75646ed20a23d912867
Dreigos <mroblesh06@gmail.com>
2019-11-02 14:55:36 -0600

    se limpio codigo y comento

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// Correccion para pruebas nueva version
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
@@ -20,21 +21,11 @@ app.use(function(req, res, next) {
     next();
 });
 
-// Body Parser
-// parse application/x-www-form-urlencoded
-
+// Otiene los datos y los formatea a json
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());   
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser({limit: '50mb'}));
-// app.use(bodyParser.json({ type: 'application/*+json' }));
-
-// Conexión a la base de datos
-// mongoose.connection.openUri('mongodb://localhost:27017/MedioAmbienteAAO', (err, res) => {
-//     if (err) throw err;
-//     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
-// });
+// Conexion a la base de datos
 mongoose.set('useCreateIndex', true)
 mongoose.connect('mongodb://localhost:27017/MedioAmbienteAAO', { useNewUrlParser: true }, (err) => {
         if (err) {
